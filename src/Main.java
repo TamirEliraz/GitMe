9e319beac908b18b2ca3aa5c827f8d5e5e60220e
TamirEliraz <80205308+TamirEliraz@users.noreply.github.com>
2021-06-02 14:51:55 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    private int x = 2;
     public static void main(String[] args){
         System.out.println("Hello world!");
         System.out.println("NEVER EVER CHANGE UI!");
